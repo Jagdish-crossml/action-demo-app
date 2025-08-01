bf7febf6e4b70c6a38e72e8e4c0d1a607c44e613
jagdish-crossml <jagdish@crossml.com>
2025-08-01 11:22:14 +0530

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Home() {
   return <h2>Home Page</h2>;
 }
 
 function About() {
-  return <h2>About Page - Loaded on Deployment </h2>;
+  return <h2>About Page - Loaded on Deployment</h2>;
 }
 
 function App() {
   return (
-    <Router basename="/my-demo-app">
+    <Router>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
